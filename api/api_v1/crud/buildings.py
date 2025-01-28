cba839e07855cd1b65736474793b304a6e10b243
Yegor <ekozinov280699@gmail.com>
2025-01-29 00:21:15 +0600

    'buildings' ORM queries

---
+++ api/api_v1/crud/buildings.py
@@ -0,0 +1,65 @@
+from typing import Sequence
+
+from geoalchemy2 import WKTElement
+from geoalchemy2.functions import ST_DWithin, ST_Transform, ST_MakeEnvelope, ST_Within
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.building_models.building import Building
+from core.schemas.building import BuildingCreate
+from utils.wkb_to_wkt import coords_as_wkt
+
+
+async def get_all_buildings(session: AsyncSession) -> Sequence[Building]:
+    stmt = select(Building).order_by(Building.id)
+    res = await session.scalars(stmt)
+    buildings = res.all()
+    for building in buildings:
+        coords_as_wkt(building)
+    return buildings
+
+
+async def create_building(session: AsyncSession, building: BuildingCreate) -> Building:
+    building.coords = WKTElement(building.coords, srid=4326)
+    building = Building(**building.model_dump())
+    session.add(building)
+    await session.commit()
+    await session.refresh(building)
+    coords_as_wkt(building)
+    return building
+
+
+async def get_buildings_in_radius(session: AsyncSession, latitude: float, longitude: float, radius: float) -> Sequence[
+    Building]:
+    point = WKTElement(f"POINT({longitude} {latitude})", srid=4326)
+
+    stmt = (
+        select(Building)
+        .filter(ST_DWithin(ST_Transform(Building.coords, 3857), ST_Transform(point, 3857), radius))
+    )
+
+    result = await session.execute(stmt)
+    buildings = result.scalars().all()
+    for building in buildings:
+        coords_as_wkt(building)
+
+    return buildings
+
+
+async def get_buildings_in_square(session: AsyncSession, points: list[list[float]]) -> Sequence[Building]:
+    points = list(points)
+    longitudes = [points[0][0], points[1][0]]
+    latitudes = [points[0][1], points[1][1]]
+
+    envelope = ST_MakeEnvelope(min(longitudes), min(latitudes), max(longitudes), max(latitudes), 4326)
+
+    query = select(Building).where(
+        ST_Within(Building.coords, envelope)
+    )
+
+    result = await session.execute(query)
+    buildings = result.scalars().all()
+    for building in buildings:
+        coords_as_wkt(building)
+
+    return buildings
